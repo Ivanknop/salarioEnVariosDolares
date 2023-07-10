7e0b981756bda9c7677caeda41af0af3e08ec504
IvanKnop <ivan.knopoff@gmail.com>
2023-07-10 19:09:02 -0300

    change api to bcra

--- dolar_converter.py
+++ dolar_converter.py
@@ -8,28 +8,29 @@ class Dolar_converter():
         self.__set_quotes()
     
     def __new_quote(self,type_of_dolar):
-        url = 'https://api-dolar-argentina.herokuapp.com/api/'+type_of_dolar
-        response = requests.get(url)
+        url = 'https://api.estadisticasbcra.com/'+type_of_dolar
+        access_token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjA1NTkxNzEsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJpdmFuLmtub3BvZmZAZ21haWwuY29tIn0.03PSad2g2IzjjKJT1nCNGKCy8uk-WJ6EXiIyGxaC9arfGMXrmMsfLdsTFovblhpbcUpOihjr6ZWJldHkjAe_NQ"
+        headers = {"Content-Type":"application/json", "Authorization": f"Bearer {access_token}"}
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             content = response.content
-            res_dict = json.loads(content.decode('utf-8'))
-            self.dolar_today[type_of_dolar] = float(res_dict['venta'])
+            res_dict = json.loads(content.decode('utf-8'))       
+            self.dolar_today[type_of_dolar] = float((res_dict[-1]["v"])
+  )
 
     def __set_quotes(self):
-        self.__new_quote('dolarblue')
-        self.__new_quote('dolaroficial')
-        self.__new_quote('dolarbolsa')
-        self.__new_quote('dolarturista')
-        self.__new_quote('mayorista')
-        self.__new_quote('contadoliqui')
+        self.__new_quote('usd')
+        self.__new_quote('usd_of')
+        self.__new_quote('usd_of_minorista')
     
+    def calculate_usd_blue(self):
+        return (self.dolar_today['usd_of'] + (self.dolar_today['usd_of']*0.35) + (self.dolar_today['usd_of']*0.30))
+
     def calculate(self):
-        self.dolar_today['dolarblue'] = round(float(self.dolar_today['pesos']) / self.dolar_today['dolarblue'],2)
-        self.dolar_today['dolaroficial'] = round(float(self.dolar_today['pesos']) / self.dolar_today['dolaroficial'],2)
-        self.dolar_today['dolarturista'] = round(float(self.dolar_today['pesos']) / self.dolar_today['dolarturista'],2)
-        self.dolar_today['contadoliqui'] = round(float(self.dolar_today['pesos']) / self.dolar_today['contadoliqui'],2)
-        self.dolar_today['dolarbolsa'] = round(float(self.dolar_today['pesos']) / self.dolar_today['dolarbolsa'],2)
-        self.dolar_today['mayorista'] = round(float(self.dolar_today['pesos']) / self.dolar_today['mayorista'],2)
+        self.dolar_today['usd'] = round(float(self.dolar_today['pesos']) / self.dolar_today['usd'],2)
+        self.dolar_today['usd_of'] = round(float(self.dolar_today['pesos']) / self.dolar_today['usd_of'],2)
+        self.dolar_today['usd_of_minorista'] = round(float(self.dolar_today['pesos']) / self.dolar_today['usd_of_minorista'],2)        
+        self.dolar_today['blue'] = round(float(self.dolar_today['pesos']) / self.calculate_usd_blue(),2)
         return self.dolar_today
 
     def show_quote(self):
